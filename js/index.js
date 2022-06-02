43b31b8914e22fb9061140ac2828c9944cd74b41
mtkn-ux <93654889+mtkn-ux@users.noreply.github.com>
2022-06-02 14:52:32 +1000

    added download button interaction

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('#downloadRes').hover(
+     function(){
+          $(this).css("background-color","#7988DA");
+     }, function(){
+          $(this).css("background-color","#00199F");
+     }
+);
